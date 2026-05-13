d22b34394fd93a5709174ce2c78e97537cdbda41
Jerome <jerparucha@gmail.com>
2026-05-14 01:44:35 +0800

    feat: hero section with name and title (Closes #5)

--- src/app/pages/profile/profile.ts
+++ src/app/pages/profile/profile.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { HeroComponent } from '../../components/hero/hero';
 
 @Component({
   selector: 'app-profile',
+  imports: [HeroComponent],
   templateUrl: './profile.html',
   styleUrl: './profile.scss',
 })
