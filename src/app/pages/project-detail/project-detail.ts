072f154c5203a646c98360dbb957b7664c716233
Jerome <jerparucha@gmail.com>
2026-05-14 02:33:21 +0800

    feat: project detail page with Steam store-style layout (Closes #13)

--- src/app/pages/project-detail/project-detail.ts
+++ src/app/pages/project-detail/project-detail.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, inject, input } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { PROJECTS } from '../../data/projects';
 
 @Component({
   selector: 'app-project-detail',
+  imports: [RouterLink],
   templateUrl: './project-detail.html',
   styleUrl: './project-detail.scss',
 })
-export class ProjectDetailPage {}
+export class ProjectDetailPage {
+  private router = inject(Router);
+
+  slug = input<string>('');
+
+  project = computed(() => PROJECTS.find(p => p.slug === this.slug()) ?? null);
+
+  constructor() {
+    effect(() => {
+      if (this.slug() && !this.project()) {
+        this.router.navigate(['/library']);
+      }
+    });
+  }
+}
